9519588f020cd1cb7aa03311aa9e9805fe3a7e5e
Faniry <faniryrandria08@gmail.com>
2023-12-22 09:29:16 +0300

    feat: create endpoint get for bigSUm fonction

--- src/main/java/com/poja/bigSum/endpoint/rest/controller/health/BigSumController.java
+++ src/main/java/com/poja/bigSum/endpoint/rest/controller/health/BigSumController.java
@@ -1,13 +1,26 @@
 package com.poja.bigSum.endpoint.rest.controller.health;
 
 import com.poja.bigSum.PojaGenerated;
-import com.poja.bigSum.endpoint.event.gen.BigSUm;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
 
 @PojaGenerated
 @RestController
 @AllArgsConstructor
 public class BigSumController {
-
+    @GetMapping("/big-sum")
+    public String calculateBigSum(@RequestParam("a") String a, @RequestParam("b") String b) {
+        try {
+            BigInteger bigA = new BigInteger(a);
+            BigInteger bigB = new BigInteger(b);
+            BigInteger sum = bigA.add(bigB);
+            return sum.toString();
+        } catch (NumberFormatException e) {
+            return "The parameters are not valid numbers.";
+        }
+    }
 }
