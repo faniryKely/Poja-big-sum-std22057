e0ca66e580750afbae2295f4f72c50961de2d5e3
Faniry <faniryrandria08@gmail.com>
2023-12-22 09:18:15 +0300

    feat: create bigSUm fonction

---
+++ src/main/java/com/poja/bigSum/endpoint/rest/controller/health/BigSumController.java
@@ -0,0 +1,13 @@
+package com.poja.bigSum.endpoint.rest.controller.health;
+
+import com.poja.bigSum.PojaGenerated;
+import com.poja.bigSum.endpoint.event.gen.BigSUm;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@PojaGenerated
+@RestController
+@AllArgsConstructor
+public class BigSumController {
+
+}
